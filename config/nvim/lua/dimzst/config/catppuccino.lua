86369ad70f6451ba38e3c55f29c6a14b10ec39f3
Dimas Muhammad Tirtadharma Pubian <dimas.m.tirtadharma@gmail.com>
2021-09-16 20:45:46 +0700

    catppuccino config

---
+++ config/nvim/lua/dimzst/config/catppuccino.lua
@@ -0,0 +1,62 @@
+local catppuccino = require("catppuccino")
+
+catppuccino.setup(
+    {
+		colorscheme = "soft_manilo",
+		transparency = true,
+		term_colors = true,
+		styles = {
+			comments = "italic",
+			functions = "italic",
+			keywords = "italic",
+			strings = "NONE",
+			variables = "NONE",
+		},
+		integrations = {
+			treesitter = true,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = "italic",
+					hints = "italic",
+					warnings = "italic",
+					information = "italic",
+				},
+				underlines = {
+					errors = "underline",
+					hints = "underline",
+					warnings = "underline",
+					information = "underline",
+				}
+			},
+			lsp_trouble = false,
+			lsp_saga = true,
+			gitgutter = false,
+			gitsigns = true,
+			telescope = true,
+			nvimtree = {
+				enabled = true,
+				show_root = true,
+			},
+			which_key = false,
+			indent_blankline = {
+				enabled = false,
+				colored_indent_levels = false,
+			},
+			dashboard = false,
+			neogit = false,
+			vim_sneak = false,
+			fern = false,
+			barbar = false,
+			bufferline = false,
+			markdown = false,
+			lightspeed = false,
+			ts_rainbow = false,
+			hop = false,
+		}
+	}
+)
+
+catppuccino.remap({},{LineNR = {fg = '#78624e'}, CursorLineNr = {fg = '#F4A261'}})
+
+vim.cmd[[colorscheme catppuccino]]
